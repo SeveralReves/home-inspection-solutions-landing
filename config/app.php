3dabbb48a18f1e089b20386bd7e4345d20a34339
Gabo Duran <gduran.ex@meatcode.cl>
2025-09-15 04:54:35 -0400

    change title

--- config/app.php
+++ config/app.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Home Handyman Solutions | Handyman in Valdosta, GA'),
 
     /*
     |--------------------------------------------------------------------------
