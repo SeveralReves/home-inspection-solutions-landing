51c046d3ce7e9caf1b72e085ec908c18395aa7cb
Gabo Duran <gduran.ex@meatcode.cl>
2025-09-25 23:31:31 -0400

    add gtm 4

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -32,5 +32,14 @@
         height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         <!-- End Google Tag Manager (noscript) -->
         @inertia
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-5Z9V6MN3VF"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-5Z9V6MN3VF');
+        </script>
     </body>
 </html>
